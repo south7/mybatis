48ec6e0b8b008a4c1b2888c74dd05dc9f0211488
wangfan <theheartwang@163.com>
2019-03-27 19:48:30 +0800

    mybatis配置入门

---
+++ src/cn/wangfan/note/Note1.java
@@ -0,0 +1,23 @@
+package cn.wangfan.note;
+/**
+ * mybatis配置及入门
+ * @author wangfan
+ *
+ */
+public class Note1 {
+/**
+ * mybatis发展历程:
+ *  ibatis:apache
+ *  2010年 ibatis->google colde,Mybatis
+ * Mybatis可以简化JDBC操作,实现数据的持久化
+ * ORM:Object Relational Mapping
+ *     person对象，person表
+ *     ORM是概念,Mybatis是ORM的一个实现，另外还有Hibernate,JPA
+ *     
+ *  配置开发步骤：
+ *  1.配置mybatis-config.xml,包含配置数据库信息和需要加载的映射文件
+ *  2.数据库表==java vo类
+ *  3.数据库表和vo映射文件：crud操作
+ *  4.单元测试
+ */
+}
