0491ba3e815fe6fe4c64c2f1f5164f8ccecd3205
wangfan <theheartwang@163.com>
2019-04-21 17:34:07 +0800

    Running MyBatis Generator With Java

---
+++ src/cn/wangfan/vo/Emp.java
@@ -0,0 +1,65 @@
+package cn.wangfan.vo;
+
+import java.util.Date;
+
+public class Emp {
+    private Integer empno;
+
+    private String ename;
+
+    private String job;
+
+    private Date hiredate;
+
+    private Double sal;
+
+    private Double comm;
+
+    public Integer getEmpno() {
+        return empno;
+    }
+
+    public void setEmpno(Integer empno) {
+        this.empno = empno;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename == null ? null : ename.trim();
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job == null ? null : job.trim();
+    }
+
+    public Date getHiredate() {
+        return hiredate;
+    }
+
+    public void setHiredate(Date hiredate) {
+        this.hiredate = hiredate;
+    }
+
+    public Double getSal() {
+        return sal;
+    }
+
+    public void setSal(Double sal) {
+        this.sal = sal;
+    }
+
+    public Double getComm() {
+        return comm;
+    }
+
+    public void setComm(Double comm) {
+        this.comm = comm;
+    }
+}
