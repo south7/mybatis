52df341c46e71d010e3ca8d9a147d301380cb094
wangfan <theheartwang@163.com>
2019-04-22 23:17:15 +0800

    mapper and mapper.xml

--- src/cn/wangfan/mapper/EmpMapper.java
+++ src/cn/wangfan/mapper/EmpMapper.java
@@ -1,30 +1,31 @@
 package cn.wangfan.mapper;
 
-import cn.wangfan.vo.Emp;
-import cn.wangfan.vo.EmpExample;
 import java.util.List;
-import org.apache.ibatis.annotations.Param;
+import java.util.Set;
 
+import cn.wangfan.vo.Emp;
+/**
+ * 动态代理方式操作mybatis的接口
+ * 接口中的方法必须遵循的预定：
+ * 方法名和EmpMapper.xml文件中标签的id值相同
+ * 方法的输入参数和EmpMapper.xml文件中标签的parameterType类型一致
+ * 方法的返回值和EmpMapper.xml文件中标签的resultType类型一致
+ * @author wangfan
+ *
+ */
 public interface EmpMapper {
-    long countByExample(EmpExample example);
+	
+    public boolean doCreate(Emp vo)throws Exception;
+	
+	public boolean doUpdate(Emp vo)throws Exception;
+	
+	public boolean doRemoveBatch(Set<Integer>ids)throws Exception;
+	
+	public Emp findById(Integer id)throws Exception;
 
-    int deleteByExample(EmpExample example);
+	public List<Emp>findAll()throws Exception;
 
-    int deleteByPrimaryKey(Integer empno);
-
-    int insert(Emp record);
-
-    int insertSelective(Emp record);
-
-    List<Emp> selectByExample(EmpExample example);
-
-    Emp selectByPrimaryKey(Integer empno);
-
-    int updateByExampleSelective(@Param("record") Emp record, @Param("example") EmpExample example);
-
-    int updateByExample(@Param("record") Emp record, @Param("example") EmpExample example);
-
-    int updateByPrimaryKeySelective(Emp record);
-
-    int updateByPrimaryKey(Emp record);
+	public List<Emp>findAllSplit(Integer currentPage,Integer lineSize,String column,String keyWord)throws Exception;
+	
+	public Integer findAllCount(String column,String keyWord)throws Exception;
 }
